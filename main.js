cb34b8a34a7484092a33404b1563c7c82ac094be
harukadoo <120182797+harukadoo@users.noreply.github.com>
2023-01-15 12:40:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("menu_btn").addEventListener("click", function() {
-    document.querySelector("header").classList.toggle("open")
+    document.querySelector("header .menu").classList.toggle("open")
 	})
 })
